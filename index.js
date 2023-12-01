bd04bf8bf33f70c167ca2f66bd843c2269029d0e
Chase Condron <chasecondron@gmail.com>
2023-12-01 03:28:09 -0500

    Now returns a promise if it contains http

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-exports.printMsg = function() {
-    console.log("This is a message from the demo package");
+async function isURL(url = ''){
+    try {
+        const original_url = new URL(url);
+        return original_url !== null && original_url.protocol.startsWith('http');
+    } catch (error) {
+        return false
+    }
 }
+
+module.exports = { isURL };
